6ab7427d4ed8938579e0c0d8f1fb73ddee1c6a7d
hamzazafarx01 <mdhamzazafariyi@gmail.com>
2024-07-18 22:38:38 +0530

    adding files
    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.multiple-text', {
+    strings: ["Frontend Developer","Embedded Developer","Content Writer"],
+typeSpeed: 30,
+backSpeed: 40,
+backDelay: 1500,
+loop: true 
+}) 
